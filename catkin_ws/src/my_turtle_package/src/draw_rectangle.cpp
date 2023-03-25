38fc73043063385a7433a4fad859a028fd889112
Hsu_Chin_Chia <chinchia120@gmail.com>
2023-03-25 23:00:58 +0800

    update

--- catkin_ws/src/my_turtle_package/src/draw_rectangle.cpp
+++ catkin_ws/src/my_turtle_package/src/draw_rectangle.cpp
@@ -1,22 +1,24 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h> 
-#define PI 3.1415926535897932384626433832795028
+#include <bits/stdc++.h>
+#define PI acos(-1)
  
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "vel_ctrl");
     ros::NodeHandle n;
-    ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
+    ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1);
     ROS_INFO("draw_rectangle start...");
+    ros::Rate loopRate(2);
+    geometry_msgs::Twist vel_cmd;
 
-    int cnt = 200000;
+    int cnt = 0;
     while(ros::ok())
     {
-        geometry_msgs::Twist vel_cmd;
         cnt++;
         //ROS_INFO("%d", cnt);
 
-        vel_cmd.linear.x = 2.0;
+        vel_cmd.linear.x = 1.5;
         vel_cmd.linear.y = 0.0;
         vel_cmd.linear.z = 0.0;
 
@@ -24,18 +26,16 @@ int main(int argc, char *argv[])
         vel_cmd.angular.y = 0.0;
         vel_cmd.angular.z = 0.0;
 
-        if(cnt > 600000 && cnt <= 736000)
+        if(cnt == 5)
         {   
+            //vel_cmd.linear.x = 0.0;
             vel_cmd.angular.z = PI;
-        }
-
-        if(cnt == 736000)
-        {
-            cnt = 200000;
+            cnt = 0;
         }
 
         vel_pub.publish(vel_cmd);
         ros::spinOnce();
+        loopRate.sleep();
     }
     return 0;
 }
