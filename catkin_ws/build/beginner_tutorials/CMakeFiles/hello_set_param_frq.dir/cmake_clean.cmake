3eebe5aa87f3480ebe6f25a73977b83fbad8c2c1
Hsu_Chin_Chia <chinchia120@gmail.com>
2023-05-12 23:02:38 +0800

    update

---
+++ catkin_ws/build/beginner_tutorials/CMakeFiles/hello_set_param_frq.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_set_param_frq.dir/src/hello_set_param_frq.cpp.o"
+  "/home/chinchia120/catkin_ws/devel/lib/beginner_tutorials/hello_set_param_frq.pdb"
+  "/home/chinchia120/catkin_ws/devel/lib/beginner_tutorials/hello_set_param_frq"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_set_param_frq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/src/beginner_tutorials/src/hello_set_param_frq.cpp
@@ -0,0 +1,14 @@
+#include <ros/ros.h>
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "hello_cpp_node");
+    ros::NodeHandle handler;
+    
+    double frq;
+    handler.getParam("/print_frq", frq);
+    
+    while (ros::ok()){
+        ROS_INFO("Hello World!");
+        ros::Duration(frq).sleep();
+    }
+}
