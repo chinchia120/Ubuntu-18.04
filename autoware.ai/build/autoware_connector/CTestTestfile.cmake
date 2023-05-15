c7322e30349eac584e94a6082fb324e37c602816
chinchia120 <71184995+chinchia120@users.noreply.github.com>
2023-05-15 22:25:49 +0800

    Delete autoware.ai/build/autoware_connector directory

--- autoware.ai/build/autoware_connector/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chinchia120/autoware.ai/src/autoware/core_perception/autoware_connector
-# Build directory: /home/chinchia120/autoware.ai/build/autoware_connector
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
