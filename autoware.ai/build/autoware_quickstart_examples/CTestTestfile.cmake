6b608dc8369d0e18c5d2b7ee59ef3e11831be91c
chinchia120 <71184995+chinchia120@users.noreply.github.com>
2023-05-15 22:30:09 +0800

    Delete autoware.ai/build/autoware_quickstart_examples directory

--- autoware.ai/build/autoware_quickstart_examples/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chinchia120/autoware.ai/src/autoware/documentation/autoware_quickstart_examples
-# Build directory: /home/chinchia120/autoware.ai/build/autoware_quickstart_examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
