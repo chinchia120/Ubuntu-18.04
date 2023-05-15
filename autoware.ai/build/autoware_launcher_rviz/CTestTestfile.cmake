f3b7b4a61ec1bfc0ddff89dcc5d8a25f475718fb
chinchia120 <71184995+chinchia120@users.noreply.github.com>
2023-05-15 22:27:41 +0800

    Delete autoware.ai/build/autoware_launcher_rviz directory

--- autoware.ai/build/autoware_launcher_rviz/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chinchia120/autoware.ai/src/autoware/utilities/autoware_launcher_rviz
-# Build directory: /home/chinchia120/autoware.ai/build/autoware_launcher_rviz
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
