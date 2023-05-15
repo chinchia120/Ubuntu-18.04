19dd11d56fd44c69b65f3bf05727d3356fbc903c
chinchia120 <71184995+chinchia120@users.noreply.github.com>
2023-05-15 22:01:50 +0800

    Delete autoware.ai/build/astar_search directory

--- autoware.ai/build/astar_search/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/chinchia120/autoware.ai/src/autoware/core_planning/astar_search/include".split(';') if "/home/chinchia120/autoware.ai/src/autoware/core_planning/astar_search/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lastar_search".split(';') if "-lastar_search" != "" else []
-PROJECT_NAME = "astar_search"
-PROJECT_SPACE_DIR = "/home/chinchia120/autoware.ai/build/astar_search/devel"
-PROJECT_VERSION = "1.12.0"
