89f7cf1a6e7311594e811c7f19c64e194ca26e4e
chinchia120 <71184995+chinchia120@users.noreply.github.com>
2023-05-15 22:02:57 +0800

    Delete autoware.ai/build/autoware_camera_lidar_calibrator directory

--- autoware.ai/build/autoware_camera_lidar_calibrator/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chinchia120/autoware.ai/src/autoware/utilities/autoware_camera_lidar_calibrator
-# Build directory: /home/chinchia120/autoware.ai/build/autoware_camera_lidar_calibrator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
