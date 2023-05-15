6f4e1f546b5e4655b07feb0d1543dacc1481244a
chinchia120 <71184995+chinchia120@users.noreply.github.com>
2023-05-15 22:27:22 +0800

    Delete autoware.ai/build/autoware_launcher directory

--- autoware.ai/build/autoware_launcher/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "python_qt_binding".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lautoware_launcher".split(';') if "-lautoware_launcher" != "" else []
-PROJECT_NAME = "autoware_launcher"
-PROJECT_SPACE_DIR = "/home/chinchia120/autoware.ai/build/autoware_launcher/devel"
-PROJECT_VERSION = "1.12.0"
